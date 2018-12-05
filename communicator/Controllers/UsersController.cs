65246dcc17ca79096d600418fcb674fa1de0002a
Filip Wilczura <filipwilczura@gmail.com>
2018-12-05 14:41:41 +0100

    Started creating CRUD API operations.

---
+++ communicator/Controllers/UsersController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using communicator.Data;
+using communicator.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace communicator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ICommunicatorRepository _repository;
+
+        public UsersController(ICommunicatorRepository repository)
+        {
+            _repository = repository;
+        }
+        
+        // GET: api/Users
+        [HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+            var users = _repository.GetAllUsers();
+
+            return users;
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult Get([FromRoute]int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = _repository.GetUserById(id);
+
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
