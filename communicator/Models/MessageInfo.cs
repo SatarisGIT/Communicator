eae8bebf76d39c686981f8e3a4219298661acd95
Filip Wilczura <filipwilczura@gmail.com>
2018-12-05 11:48:59 +0100

    Created models for database.

---
+++ communicator/Models/MessageInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace communicator.Models
+{
+    public class MessageInfo
+    {
+        public int ID { get; set; }
+
+        [Key]
+        [ForeignKey("User")]
+        public int SenderID { get; set; }
+
+        [Key]
+        [ForeignKey("User")]
+        public int ReceiverID { get; set; }
+
+        [Key]
+        [ForeignKey("Message")]
+        public int MessageID { get; set; }
+
+        public bool IsRead { get; set; }
+
+        public DateTime DateSend { get; set; }
+
+
+    }
+}
