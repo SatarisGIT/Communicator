59fda231d25eec6b444b2f8b2fa28163da73a10f
Filip Wilczura <filipwilczura@gmail.com>
2018-12-19 22:19:15 +0100

    Added test project

---
+++ communicator.Tests/UsersControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using communicator.Controllers;
+using communicator.Data.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace communicator.Tests
+{
+    public class UsersControllerTest
+    {
+        private UsersController _controller;
+        private IRepositoryWrapper _repository;
+        private ILogger<UsersController> _logger;
+
+        public UsersControllerTest()
+        {
+            _controller = new UsersController(_repository, _logger);
+        }
+
+        public void GetUsers_WhenCalled_ReturnsIEnumerableUsers()
+        {
+            //Act
+            var okResult = _controller.GetUsers();
+        }
+    }
+}
